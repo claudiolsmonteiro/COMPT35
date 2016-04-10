1adce7a9106b3b735e653702af82fffa27fe3950
David-Caminha <david.caminha94@gmail.com>
2016-04-10 18:50:57 +0100

    Versao inicial do lexer

--- MarkdownLexer.g4
+++ MarkdownLexer.g4
@@ -1,16 +1,102 @@
 lexer grammar MarkdownLexer;
 
-POSITION
+
+SIMPLETEXT //Match any text without markdown sintax
 :
-	'right'
-	| 'left'
-	| 'center'
+	(
+		~('\\'|'`'|'*'|'_'|'['|']'|'('|')'|'#'|'+'|'-'|'.'|'!')+ 
+		| (BACKSLASH (
+			BACKSLASH 
+			| BACKTICK 
+			| ASTERISK 
+			| UNDERSCORE
+			| LBRACKET
+			| RBRACKET
+			| LPAREN
+			| RPAREN
+			| HASH
+			| PLUS
+			| MINUS
+			| DOT
+			| EXCLAMATIONMARK
+		))+
+	)+
 ;
 
-// match lower-case identifiers
-LETTER
+BOLD
 :
-	[a-zA-Z]+
+	ASTERISK ASTERISK (SIMPLETEXT | ITALICS | STRIKETHROUGH) ASTERISK ASTERISK
+;
+
+ITALICS
+:
+	UNDERSCORE (SIMPLETEXT | BOLD | STRIKETHROUGH) UNDERSCORE
+;
+
+STRIKETHROUGH
+:
+	TILDE TILDE (SIMPLETEXT | ITALICS | BOLD) TILDE TILDE
+;
+
+HEADER
+:
+	HASH+ (SIMPLETEXT | ITALICS | BOLD | STRIKETHROUGH)
+;
+
+//CHARS
+BACKSLASH
+:
+	'\\'
+;
+
+TABLESEPARATOR
+:
+	'|'
+;
+
+EXCLAMATIONMARK
+:
+	'!'
+;
+
+QUOTE
+:
+	'"'
+;
+
+HASH
+:
+	'#'
+;
+
+SLASH
+:
+	'/'
+;
+
+LPAREN
+:
+	'('
+;
+
+RPAREN
+:
+	')'
+;
+
+LBRACKET
+:
+	'['
+;
+
+RBRACKET
+:
+	']'
+;
+
+DOT
+:
+	'.'
 ;
 
 COLON
@@ -18,19 +104,49 @@ COLON
 	':'
 ;
 
+COMMA
+:
+	','
+;
+
+SEMICOLON
+:
+	';'
+;
+
+UNDERSCORE
+:
+	'_'
+;
+
+PLUS
+:
+	'+'
+;
+
 MINUS
 :
 	'-'
 ;
 
-TABLE
+ASTERISK
 :
-	'table'
+	'*'
 ;
 
-TABLESEPARATOR
+TILDE
 :
-	'|'
+	'~'
+;
+
+BACKTICK
+:
+	'`'
+;
+
+GREATERTHAN
+:
+	'>'
 ;
 
 WS : [ \t\r\n]+ -> skip ; // skip spaces, tabs, newlines
--- target/generated-sources/antlr4/MarkdownParser.java
+++ target/generated-sources/antlr4/MarkdownParser.java
@@ -16,9 +16,16 @@ public class MarkdownParser extends Parser {
 	protected static final PredictionContextCache _sharedContextCache =
 		new PredictionContextCache();
 	public static final int
-		TABLE=5, POSITION=1, LETTER=2, COLON=3, WS=7, MINUS=4, TABLESEPARATOR=6;
+		QUOTE=9, UNDERSCORE=20, HEADER=5, MINUS=22, BACKSLASH=6, ITALICS=3, SEMICOLON=19, 
+		BOLD=2, BACKTICK=25, WS=27, COMMA=18, EXCLAMATIONMARK=8, SLASH=11, LPAREN=12, 
+		DOT=16, LBRACKET=14, ASTERISK=23, RBRACKET=15, COLON=17, RPAREN=13, HASH=10, 
+		STRIKETHROUGH=4, TILDE=24, SIMPLETEXT=1, GREATERTHAN=26, TABLESEPARATOR=7, 
+		PLUS=21;
 	public static final String[] tokenNames = {
-		"<INVALID>", "POSITION", "LETTER", "':'", "'-'", "'table'", "'|'", "WS"
+		"<INVALID>", "SIMPLETEXT", "BOLD", "ITALICS", "STRIKETHROUGH", "HEADER", 
+		"'\\'", "'|'", "'!'", "'\"'", "'#'", "'/'", "'('", "')'", "'['", "']'", 
+		"'.'", "':'", "','", "';'", "'_'", "'+'", "'-'", "'*'", "'~'", "'`'", 
+		"'>'", "WS"
 	};
 	public static final int
 		RULE_r = 0, RULE_table = 1, RULE_secondROW = 2;
@@ -88,12 +95,12 @@ public class MarkdownParser extends Parser {
 			return getRuleContext(SecondROWContext.class,0);
 		}
 		public List<TerminalNode> TABLESEPARATOR() { return getTokens(MarkdownParser.TABLESEPARATOR); }
-		public List<TerminalNode> LETTER() { return getTokens(MarkdownParser.LETTER); }
-		public TerminalNode LETTER(int i) {
-			return getToken(MarkdownParser.LETTER, i);
-		}
+		public List<TerminalNode> SIMPLETEXT() { return getTokens(MarkdownParser.SIMPLETEXT); }
 		public TerminalNode TABLESEPARATOR(int i) {
 			return getToken(MarkdownParser.TABLESEPARATOR, i);
+		}
+		public TerminalNode SIMPLETEXT(int i) {
+			return getToken(MarkdownParser.SIMPLETEXT, i);
 		}
 		public TableContext(ParserRuleContext parent, int invokingState) {
 			super(parent, invokingState);
@@ -122,14 +129,14 @@ public class MarkdownParser extends Parser {
 			do {
 				{
 				{
-				setState(8); match(LETTER);
+				setState(8); match(SIMPLETEXT);
 				setState(9); match(TABLESEPARATOR);
 				}
 				}
 				setState(12); 
 				_errHandler.sync(this);
 				_la = _input.LA(1);
-			} while ( _la==LETTER );
+			} while ( _la==SIMPLETEXT );
 			setState(14); secondROW();
 			setState(17); 
 			_errHandler.sync(this);
@@ -137,14 +144,14 @@ public class MarkdownParser extends Parser {
 			do {
 				{
 				{
-				setState(15); match(LETTER);
+				setState(15); match(SIMPLETEXT);
 				setState(16); match(TABLESEPARATOR);
 				}
 				}
 				setState(19); 
 				_errHandler.sync(this);
 				_la = _input.LA(1);
-			} while ( _la==LETTER );
+			} while ( _la==SIMPLETEXT );
 			}
 		}
 		catch (RecognitionException re) {
@@ -228,16 +235,16 @@ public class MarkdownParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\t#\4\2\t\2\4\3\t"+
+		"\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\35#\4\2\t\2\4\3\t"+
 		"\3\4\4\t\4\3\2\3\2\3\3\3\3\6\3\r\n\3\r\3\16\3\16\3\3\3\3\3\3\6\3\24\n"+
 		"\3\r\3\16\3\25\3\4\5\4\31\n\4\3\4\6\4\34\n\4\r\4\16\4\35\3\4\3\4\3\4\3"+
 		"\4\2\2\5\2\4\6\2\2#\2\b\3\2\2\2\4\f\3\2\2\2\6\30\3\2\2\2\b\t\5\4\3\2\t"+
-		"\3\3\2\2\2\n\13\7\4\2\2\13\r\7\b\2\2\f\n\3\2\2\2\r\16\3\2\2\2\16\f\3\2"+
-		"\2\2\16\17\3\2\2\2\17\20\3\2\2\2\20\23\5\6\4\2\21\22\7\4\2\2\22\24\7\b"+
+		"\3\3\2\2\2\n\13\7\3\2\2\13\r\7\t\2\2\f\n\3\2\2\2\r\16\3\2\2\2\16\f\3\2"+
+		"\2\2\16\17\3\2\2\2\17\20\3\2\2\2\20\23\5\6\4\2\21\22\7\3\2\2\22\24\7\t"+
 		"\2\2\23\21\3\2\2\2\24\25\3\2\2\2\25\23\3\2\2\2\25\26\3\2\2\2\26\5\3\2"+
-		"\2\2\27\31\7\5\2\2\30\27\3\2\2\2\30\31\3\2\2\2\31\33\3\2\2\2\32\34\7\6"+
-		"\2\2\33\32\3\2\2\2\34\35\3\2\2\2\35\33\3\2\2\2\35\36\3\2\2\2\36\37\3\2"+
-		"\2\2\37 \7\5\2\2 !\7\b\2\2!\7\3\2\2\2\6\16\25\30\35";
+		"\2\2\27\31\7\23\2\2\30\27\3\2\2\2\30\31\3\2\2\2\31\33\3\2\2\2\32\34\7"+
+		"\30\2\2\33\32\3\2\2\2\34\35\3\2\2\2\35\33\3\2\2\2\35\36\3\2\2\2\36\37"+
+		"\3\2\2\2\37 \7\23\2\2 !\7\t\2\2!\7\3\2\2\2\6\16\25\30\35";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
